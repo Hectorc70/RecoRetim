ee0764af9209aa4e6e40f137eef89913414fa0e8
Hector <hecgarcia.n@gmail.com>
2020-01-09 17:28:22 -0600

    se inicio el subproceso para recuperar archivos excel de los datos

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from tkinter.filedialog import askdirectory
 
 from carpetas import CarpetaNomina
+from archivos.rutas import Rutas
 
 
 def obtener_rutas_archivos():
     CarpetaNomina(askdirectory())
-    
+
+def obtener_documentos():
+    Rutas(askdirectory())
+
+
+#obtener_rutas_archivos()
+
 obtener_rutas_archivos()
 
