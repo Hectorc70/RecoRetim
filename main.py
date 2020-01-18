8a3051a310d5f0fb9408c0f8db86e176154cd94b
Hector <hecgarcia.n@gmail.com>
2020-01-17 18:53:55 -0600

    pruebas modelo Excel cambiado

--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 from tkinter.filedialog import askdirectory
 
 from carpetas import CarpetaNomina
-#from archivos.lectura import ArchivoIQ, ReporteSap
 
-def obtener_rutas_archivos():
-    CarpetaNomina(askdirectory())
-
-    
-def lectura_excel():
-    #iq = ArchivoIQ()
-    #iq.extraer_control()
-    #iq.extraer_ccn_1401()
-    #iq.extraer_ccn_1409()
-    sap = ReporteSap()
-    sap.obtener_control()
-
-obtener_rutas_archivos()
-
-#lectura_excel()
 
 
 class ArchivoLayout():
