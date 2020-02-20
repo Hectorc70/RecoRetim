cde51dfa4c3d24fcae2b1519b9a089e8fefebdce
Hector Garcia Nieves <hecgarcia.n@gmail.com>
2020-02-20 14:54:41 -0600

    Revert "TEMPORAL"
    This reverts commit 0e031b9f2e31819940989fd5df2a5bc546d61250.

--- timbres_txt/ordinaria.py
+++ timbres_txt/ordinaria.py
@@ -22,66 +22,25 @@ class NominaOrdinariaBase(Nomina, Nomina4):
 		for ruta, carpetas, documentos in os.walk(self.ruta_nominas, topdown = True):
 
 			for tipo_de_nomina in carpetas:
-				ruta_completa_nomina = ruta.replace("/", "\\") + "\\" + tipo_de_nomina
+					ruta_completa_nomina = ruta.replace("/", "\\") + "\\" + tipo_de_nomina
 
-				if tipo_de_nomina.split("_")[0] == "ORDINARIA":
+					if tipo_de_nomina.split("_")[0] == "ORDINARIA":
 
-					nom1 = Nomina(ruta_completa_nomina)
+						nom1 = Nomina(ruta_completa_nomina)
 
-					# obtener rutas de xml y txt
-					nom1_timbres = nom1.recuperar_timbres()
-					nom1_cfdi    = nom1.recuperar_txt()
+						# obtener rutas de xml y txt
+						nom1_timbres = nom1.recuperar_timbres()
+						nom1_cfdi    = nom1.recuperar_txt()
 
-					nom4 = Nomina4(ruta_completa_nomina)
-					nom4_timbres = nom4.recuperar_timbres_nom4()
-					nom4_cfdi    = nom4.recuperar_txt_nom4()       
+						nom4 = Nomina4(ruta_completa_nomina)
+						nom4_timbres = nom4.recuperar_timbres_nom4()
+						nom4_cfdi    = nom4.recuperar_txt_nom4()       
 
-					self.archivos_nom["nom1_timbres"] = nom1_timbres
-					self.archivos_nom["nom1_cfdi"]    = nom1_cfdi
+						self.archivos_nom["nom1_timbres"] = nom1_timbres
+						self.archivos_nom["nom1_cfdi"]    = nom1_cfdi
 
-					self.archivos_nom["nom4_timbres"] = nom4_timbres
-					self.archivos_nom["nom4_cfdi"]    = nom4_cfdi
-				
-				nomina_complementaria = NominaComplementaria()
-						
-					
-
-
-
-
-							
+						self.archivos_nom["nom4_timbres"] = nom4_timbres
+						self.archivos_nom["nom4_cfdi"]    = nom4_cfdi 
 
 		return self.archivos_nom
 			
-class NominaComplementaria(NominaOrdinariaBase):
-
-	
-
-	def recuperar_nom(self):
-		
-		
-
-		for ruta, carpetas, documentos in os.walk(self.ruta_nominas, topdown = True):
-
-			for tipo_de_nomina in carpetas:
-				ruta_completa_nomina = ruta.replace("/", "\\") + "\\" + tipo_de_nomina
-
-				if tipo_de_nomina.split("_")[0] == "COMPLEMENTARIA":
-
-					nom_comple1 = Nomina(ruta_completa_nomina)
-					nom1_timbres = nom_comple1.recuperar_timbres()
-					nom1_cfdi    = nom_comple1.recuperar_txt()
-
-					nom_comple4 = Nomina4(ruta_completa_nomina)
-					nom4_timbres = nom_comple4.recuperar_timbres_nom4()
-					nom4_cfdi    = nom_comple4.recuperar_txt_nom4()  
-
-					
-					self.archivos_nom["nom1_timbres"] = nom1_timbres
-					self.archivos_nom["nom1_cfdi"]    = nom1_cfdi
-
-					self.archivos_nom["nom4_timbres"] = nom4_timbres
-					self.archivos_nom["nom4_cfdi"]    = nom4_cfdi
-				
-				else:
-					continue
